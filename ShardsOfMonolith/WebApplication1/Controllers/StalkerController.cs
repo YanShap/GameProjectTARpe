d192d11f5b910402b9c08caa6d886a0f054f6ff0
YanShap <158562027+YanShap@users.noreply.github.com>
2024-10-17 12:31:28 +0300

    123
    commit

--- ShardsOfMonolith/WebApplication1/Controllers/StalkerController.cs
+++ ShardsOfMonolith/WebApplication1/Controllers/StalkerController.cs
@@ -1,14 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShardsOfMonolith.Date;
 
 namespace ShardsOfMonolith.Controllers
 {
-    public class StalkerController : Controller
+    public class StalkersController : Controller
     {
         /*
-         * Stalker Controller 
+         * StalkerController controls all functions for stalker, including, missinons 
          */
+        private readonly ShardsOfMonolithContext _context;
+        private readonly IStalkerServices _stalkerServices;
+
+        public StalkersController(ShardsOfMonolithContext context, IStalkerServices stalkerServices)
+        {
+            _context = context;
+            _stalkerServices = stalkerServices
+
+        }
         public IActionResult Index()
         {
+            var resultingInventory = _context.Stalkers
+                .OrderByDescending (y => y.StalkerLevel)
+                .Select( x => new StalkerIndexViewModel
+                {
+                    ID = x.ID,
+                    StalkerName = x.StalkerName,
+                    StalkerType = x.StalkerType,
+                    StalkerLevel = x.StalkerLevel,
+                });
             return View();
         }
     }
