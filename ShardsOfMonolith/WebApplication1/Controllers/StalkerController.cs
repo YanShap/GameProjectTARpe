3deda3b0e80f1c4cccee9cddf75a83d77bb2480e
YanShap <158562027+YanShap@users.noreply.github.com>
2024-10-17 12:07:16 +0300

    Read Disc
    Finish smt

---
+++ ShardsOfMonolith/WebApplication1/Controllers/StalkerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShardsOfMonolith.Controllers
+{
+    public class StalkerController : Controller
+    {
+        /*
+         * Stalker Controller 
+         */
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
